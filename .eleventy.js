a2fe0adaf98096aaf9dd6d38945f67d5c6c42066
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2020-11-23 10:48:50 +0100

    system updates and image blurry-effect

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,9 @@
 const htmlmin = require("html-minifier");
-const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
+const Image = require("@11ty/eleventy-img");
+const sharp = require("sharp");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(lazyImagesPlugin, {
-    preferNativeLazyLoad: true,
-  });
-
   eleventyConfig.addWatchTarget("./_tmp/style.css");
-
   eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
   eleventyConfig.addPassthroughCopy({
     "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
@@ -17,6 +13,55 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
+  });
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", async (src, alt) => {
+    if (!alt) {
+      throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+    }
+
+    let stats = await Image(src, {
+      widths: [25, 320, 640, 960, 1200, 1800, 2400],
+      formats: ["jpeg", "webp"],
+      urlPath: "/images/",
+      outputDir: "./dist/images/",
+    });
+
+    let lowestSrc = stats["jpeg"][0];
+
+    const placeholder = await sharp(lowestSrc.outputPath)
+      .resize({ fit: sharp.fit.inside })
+      .blur()
+      .toBuffer();
+
+    const base64Placeholder = `data:image/png;base64,${placeholder.toString(
+      "base64"
+    )}`;
+
+    const srcset = Object.keys(stats).reduce(
+      (acc, format) => ({
+        ...acc,
+        [format]: stats[format].reduce(
+          (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+          ""
+        ),
+      }),
+      {}
+    );
+
+    const source = `<source type="image/webp" data-srcset="${srcset["webp"]}" >`;
+
+    const img = `<img
+      class="lazy"
+      alt="${alt}"
+      src="${base64Placeholder}"
+      data-src="${lowestSrc.url}"
+      data-sizes='(min-width: 1024px) 1024px, 100vw'
+      data-srcset="${srcset["jpeg"]}"
+      width="${lowestSrc.width}"
+      height="${lowestSrc.height}">`;
+
+    return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`;
   });
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
@@ -45,7 +90,7 @@ module.exports = function (eleventyConfig) {
       data: "data",
     },
     passthroughFileCopy: true,
-    templateFormats: ["html", "njk", "md", "11ty.js"],
+    templateFormats: ["html", "njk"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
   };
