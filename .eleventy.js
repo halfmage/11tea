f2dad384fa6f3d434fbe1c3d416cdc17120ddfab
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-07-08 15:53:51 +0200

    dependencies updates + prep for v2

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,34 @@
 const htmlmin = require("html-minifier");
 const Image = require("@11ty/eleventy-img");
-const sharp = require("sharp");
+
+async function imageShortcode(src, alt, className, sizes = "100vw") {
+  if(alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [200, 400, 800, 1200],
+    formats: ['webp', 'jpeg'],
+    outputDir: "./dist/img/",
+  });
+
+  let lowsrc = metadata.jpeg[0];
+
+  return `<picture>
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+    }).join("\n")}
+      <img
+        src="${lowsrc.url}"
+        width="${lowsrc.width}"
+        height="${lowsrc.height}"
+        alt="${alt}"
+        class="${className}"
+        loading="lazy"
+        decoding="async">
+    </picture>`;
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./_tmp/style.css");
@@ -15,57 +43,7 @@ module.exports = function (eleventyConfig) {
     return String(Date.now());
   });
 
-  eleventyConfig.addNunjucksAsyncShortcode(
-    "image",
-    async (src, alt, className) => {
-      if (!alt) {
-        throw new Error(`Missing \`alt\` on Image from: ${src}`);
-      }
-
-      let stats = await Image(src, {
-        widths: [25, 320, 640, 960, 1200, 1800, 2400],
-        formats: ["jpeg", "webp"],
-        urlPath: "/images/",
-        outputDir: "./dist/images/",
-      });
-
-      let lowestSrc = stats["jpeg"][0];
-
-      const placeholder = await sharp(lowestSrc.outputPath)
-        .resize({ fit: sharp.fit.inside })
-        .blur()
-        .toBuffer();
-
-      const base64Placeholder = `data:image/png;base64,${placeholder.toString(
-        "base64"
-      )}`;
-
-      const srcset = Object.keys(stats).reduce(
-        (acc, format) => ({
-          ...acc,
-          [format]: stats[format].reduce(
-            (_acc, curr) => `${_acc} ${curr.srcset} ,`,
-            ""
-          ),
-        }),
-        {}
-      );
-
-      const source = `<source type="image/webp" data-srcset="${srcset["webp"]}" >`;
-
-      const img = `<img
-      class="lazy ${className}"
-      alt="${alt}"
-      src="${base64Placeholder}"
-      data-src="${lowestSrc.url}"
-      data-sizes='(min-width: 1024px) 1024px, 100vw'
-      data-srcset="${srcset["jpeg"]}"
-      width="${lowestSrc.width}"
-      height="${lowestSrc.height}">`;
-
-      return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`;
-    }
-  );
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (
