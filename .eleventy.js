cc0ccdbeda28184dd0d00b15b56cf85d4657852f
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-07-09 09:56:36 +0200

    performance updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,67 +1,62 @@
-const htmlmin = require("html-minifier");
-const Image = require("@11ty/eleventy-img");
+const Image = require("@11ty/eleventy-img")
+const htmlmin = require('html-minifier')
+const now = String(Date.now())
 
-async function imageShortcode(src, alt, className, sizes = "100vw") {
-  if(alt === undefined) {
-    // You bet we throw an error on missing alt (alt="" works okay)
-    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-  }
-
-  let metadata = await Image(src, {
-    widths: [200, 400, 800, 1200],
-    formats: ['webp', 'jpeg'],
-    outputDir: "./_site/img/",
+async function imageShortcode(src, alt, sizes = "100vw") {
+  let metadata = await Image(`${src}`, {
+    widths: [600, 1000, 1600],
+    formats: ["avif", "jpeg"],
+    urlPath: "/img/",
+    outputDir: "./_site/img/"
   });
-
-  let lowsrc = metadata.jpeg[0];
-
-  return `<picture>
-    ${Object.values(metadata).map(imageFormat => {
-      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-    }).join("\n")}
-      <img
-        src="${lowsrc.url}"
-        width="${lowsrc.width}"
-        height="${lowsrc.height}"
-        alt="${alt}"
-        class="${className}"
-        loading="lazy"
-        decoding="async">
-    </picture>`;
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+  return Image.generateHTML(metadata, imageAttributes);
 }
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./_tmp/style.css");
-  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
+
+  eleventyConfig.setUseGitIgnore(false)
+
+  eleventyConfig.addPassthroughCopy("images")
+  eleventyConfig.addPassthroughCopy("fonts")
+  eleventyConfig.addPassthroughCopy("site.webmanifest")
+
+  eleventyConfig.addWatchTarget('./_tmp/style.css')
+
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+  
   eleventyConfig.addPassthroughCopy({
-    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
-  });
-  eleventyConfig.addPassthroughCopy("src/assets/images");
-  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+    './node_modules/alpinejs/dist/alpine.js': './js/alpine.js',
+  })
 
-  eleventyConfig.addShortcode("version", function () {
-    return now;
-  });
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  })
 
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (
       process.env.ELEVENTY_PRODUCTION &&
       outputPath &&
-      outputPath.endsWith(".html")
+      outputPath.endsWith('.html')
     ) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
       });
-      return minified;
+      return minified
     }
-    return content;
+
+    return content
   });
 
   return {
     htmlTemplateEngine: "njk",
   };
-};
+}
